4507795fb63bbb2c2e48c02cfb1e2d77ff398f81
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2026-03-22 01:00:03 +0100

    fix(repo): remove portfolios (#20)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,53 +1,46 @@
 const scopes = [
-  // content_generation
-  "content_generation/readme_diy",
+        // content_generation
+        "content_generation/readme_diy",
 
-  // emulation
-  "emulation/zmu",
+        // emulation
+        "emulation/zmu",
 
-  // gpu_computing
-  "gpu_computing/vulkan-zig",
-  "gpu_computing/wheat",
-  "gpu_computing/zig_tests",
+        // gpu_computing
+        "gpu_computing/vulkan-zig",
+        "gpu_computing/wheat",
+        "gpu_computing/zig_tests",
 
-  // graph_walking
-  "graph_walking/wdw",
+        // graph_walking
+        "graph_walking/wdw",
 
-  // node_tooling
-  "node_tooling/silo",
+        // node_tooling
+        "node_tooling/silo",
 
-  // packaging
-  "packaging/celeste_flake",
-  "packaging/snowblock",
+        // packaging
+        "packaging/celeste_flake",
+        "packaging/snowblock",
 
-  // portfolios
-  "portfolios/portfolio",
-  "portfolios/portfolio2",
-  "portfolios/portfolio3",
-  "portfolios/portfolio4",
-  "portfolios/portfolio5-svelte",
+        // scraping
+        "scraping/gbscraper",
+        "scraping/gbscraper_old",
 
-  // scraping
-  "scraping/gbscraper",
-  "scraping/gbscraper_old",
+        // system_tooling
+        "system_tooling/isod",
 
-  // system_tooling
-  "system_tooling/isod",
+        // webapps
+        "webapps/instanc.es",
 
-  // webapps
-  "webapps/instanc.es",
-
-  // repo-wide
-  "repo", // root-level changes
-  "ci",
-  "deps", // root-level deps updates
-  "release", // release-please bot
+        // repo-wide
+        "repo", // root-level changes
+        "ci",
+        "deps", // root-level deps updates
+        "release", // release-please bot
 ];
 
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "scope-enum": [2, "always", scopes],
-    "scope-empty": [2, "never"],
-  },
+        extends: ["@commitlint/config-conventional"],
+        rules: {
+                "scope-enum": [2, "always", scopes],
+                "scope-empty": [2, "never"],
+        },
 };
